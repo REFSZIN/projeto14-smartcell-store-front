6749d6947f710b8d8be4a19e6a64e81c9ea1ab3e
refszin <refwire@gmail.com>
2022-09-14 20:32:11 -0300

    Revert "Build: Context"
    This reverts commit be2a2f49a18da137a984894470ae1aaee37a6753.

--- src/App.js
+++ src/App.js
@@ -4,32 +4,27 @@ import ResetCSS from './assets/css/global.js';
 import UserContext from "./UserContext";
 
 import { UserProvider } from './UserContext.js';
-import Login from "./components/Sign/Sign.js";
-import Store from "./components/Store/Store.js";
-import SignUp from "./components/SignUp/SignUp.js";
-import Cart from "./components/Cart/Cart.js";
-import CheckOut from "./components/CheckOut/CheckOut.js";
-import PrivatePage from "./components/PrivatePage.js";
+
+import  from "./Pages/.js";
+
+
 
 export default function App (){
-    return (
-        <UserContext.Provider value={UserContext}>
-            <UserProvider>
-                <BrowserRouter>
-                <ResetCSS/>
-                    <Routes>
-                        <Route path="/" element={<Store />}/>
-                        <Route path="/login" element={<Login />}/>
-                        <Route path="/cadatro" element={<SignUp />}/>
-                        <Route path="/carrinho" element={<Cart />}/>
-                        <Route path="/checkout" element={
-                            <PrivatePage>
-                                <CheckOut/>
-                            </PrivatePage>
-                        }/>
-                    </Routes> 
-                </BrowserRouter>
-            </UserProvider>
-        </UserContext.Provider>
+return (
+    <UserContext.Provider value={UserContext}>
+        <UserProvider>
+            <BrowserRouter>
+            <ResetCSS/>
+                <Routes>
+                    <Route path="/" element={< />}/>
+                    <Route path="/login" element={< />}/>
+                    <Route path="/home" element={< />}/>
+                    <Route path="/cadastro" element={< />}/>
+                    <Route path="/carrinho" element={</>}/>
+                    <Route path="/checkaout" element={</>}/>
+                </Routes> 
+            </BrowserRouter>
+        </UserProvider>
+    </UserContext.Provider>
     );
 };
